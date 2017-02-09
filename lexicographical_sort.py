04f2a9975733c2c40eb996a069166036994ace9d
majordoobie <sgtmajordoobie@live.com>
2017-02-09 13:06:54 -0500

    lexicographical name

--- lexicographical_sort.py
+++ lexicographical_sort.py
@@ -2,6 +2,8 @@
 Make sure you set the right permissions to run this script 'chmod +x'
 then call this script with the full path of your text file
 example: "./ballsack.py /root/shitfile"
+
+Sorts a list into lexicographical order 
 """
 !/usr/bin/python
 
